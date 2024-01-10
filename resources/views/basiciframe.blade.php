4a59cf712d4124d20c73bbdfafccaa3df5cc7f4e
sifrious <mmebit@icloud.com>
2024-01-10 13:16:09 -0500

    first endpoint

---
+++ resources/views/basiciframe.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <form id="estimate">
+        <label for="estimateSize">Estimate:</label>
+        <select name="size" id="estimateSize">
+            <option value="small">Small 👕</option>
+            <option value="medium">Medium 👚</option>
+            <option value="large">Large 👔</option>
+            <option value="x-large">Extra Large 👖</option>
+        </select>
+        <button type="submit" class="mod-primary">Save</button>
+    </form>
+    <script src="https://p.trellocdn.com/power-up.min.js"></script>
+    <script src={{ 'js/scriptbasic.js' }}></script>
+</body>
+
+</html>
